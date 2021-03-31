122196aa4deb7c82dd9be8f2f900be628d3f2172
MuratTuz <murattuzlali@gmail.com>
2021-03-31 11:31:40 +0200

    readFile service has been added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 
 import './App.css';
-import * as fs from 'fs';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import { useState } from 'react';
+import { Spinner } from "react-bootstrap";
+
 import { Container } from 'react-bootstrap';
 import Person from './components/personal/person';
 import Friend from './components/friends/friend';
 import Child from './components/children/child';
 
+import readFile from './services/readFile';
+import fileURL from './foreigners.json';
+
 
 function App() {
+  console.log(fileURL.length);
+  const [fileJSON, setfileJSON] = useState('');
+  readFile(fileURL).then(data => setfileJSON(data));
+  console.log('fileJSon ' + fileJSON);
+  console.log(fileURL);
 
-  const file = fs.readFileSync(process.env.PUBLIC_URL + '/foreigners.json');
-
-  const fileData = JSON.parse(file);
-  fileData.map((element, index) => {
-    return (
-      <Container kex={index}>
-        <Person data={element} />
-        <Child data={element.children} />
-        <Friend data={element.friends} />
-      </Container>
-    )
-  })
+  fileURL ?
+    fileURL.map((element, index) => {
+      return (
+        <Container kex={index}>
+          <Person data={element} />
+          <Child data={element.children} />
+          <Friend data={element.friends} />
+        </Container>
+      )
+    })
+    :
+    Array.from([1]).map(e => {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      )
+    })
 
 }
 
